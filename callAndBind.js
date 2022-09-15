ced0119427f02705f69944f9702828d679081940
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2022-09-15 19:39:18 +0530

    Currying

--- callAndBind.js
+++ callAndBind.js
@@ -24,11 +24,29 @@
 // var bound=addList.bind(obj);
 // console.log(bound(arr));
 
-let Student={age:20}
+// let Student={age:20}
 
-let printAge = function age() {
-      return this.age
+// let printAge = function age() {
+//       return this.age
+// }
+
+// let e = printAge.bind(Student)
+// console.log(e())
+
+
+
+let multiply=function(x,y){
+    console.log(x*y);
 }
 
-let e = printAge.bind(Student)
-console.log(e())
+
+let multiplyByTwo=multiply.bind(this,2,3);
+multiplyByTwo(3,4);
+let multiply=function(x){
+    return function(y){
+        console.log(x*y);
+    }
+}
+
+let multiplyByTwo=multiply(2);
+multiplyByTwo(5);
