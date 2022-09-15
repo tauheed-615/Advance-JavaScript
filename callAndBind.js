0277b4e2d7cd1e0195e16ad3e47222f592822107
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2022-09-15 19:36:40 +0530

    Bind

--- callAndBind.js
+++ callAndBind.js
@@ -1,3 +1,17 @@
+// var obj={
+//     num:2
+// };
+
+// var addList=function(a,b,c,d){
+//     return this.num+a+b+c+d;
+// };
+// // console.log(addList.call(obj,1,2,3,1));
+
+
+// let arr=[1,2,3,1];
+// console.log(addList.apply(obj,arr));
+
+
 var obj={
     num:2
 };
@@ -5,8 +19,7 @@ var obj={
 var addList=function(a,b,c,d){
     return this.num+a+b+c+d;
 };
-// console.log(addList.call(obj,1,2,3,1));
-
-
 let arr=[1,2,3,1];
-console.log(addList.apply(obj,arr));
+
+var bound=addList.bind(obj);
+console.log(bound(arr));
